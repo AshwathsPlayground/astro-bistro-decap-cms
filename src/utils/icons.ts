1798837a1af847b612feb3d6524abad5910fca44
AchuAshwath <achuashwath88@gmail.com>
2026-06-13 15:07:59 +0530

    feat: add dynamic Lucide React icon helper map

---
+++ src/utils/icons.ts
@@ -0,0 +1,21 @@
+import {
+  SparklesIcon,
+  ChefHat,
+  Users,
+  Trophy,
+  Clock8Icon,
+  MapPinIcon,
+  Mail,
+  PhoneIcon
+} from 'lucide-react'
+
+export const iconMap: Record<string, any> = {
+  SparklesIcon,
+  ChefHat,
+  Users,
+  Trophy,
+  Clock8Icon,
+  MapPinIcon,
+  Mail,
+  PhoneIcon
+}
